09e743609314ca1078691451bd590582e010e2b4
shengyu <shengyu7697@gmail.com>
2018-03-15 19:22:41 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Cplusplus-tutorial
+C++ 實戰範例教學, 與 STL 的應用.
+
+## 參考
+[http://larry850806.github.io/2016/06/06/STL1/](http://larry850806.github.io/2016/06/06/STL1/)  
+map  
+[http://mropengate.blogspot.tw/2015/12/cc-map-stl.html](http://mropengate.blogspot.tw/2015/12/cc-map-stl.html)  
---
+++ STL/map/map.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(){
+
+    // declare container and iterator
+    map<int, string> mapStudent;
+    map<int, string>::iterator iter;
+    map<int, string>::reverse_iterator iter_r;
+
+    // insert element
+    mapStudent.insert(pair<int, string>(000, "Jack"));
+
+    mapStudent[123] = "Tom";
+    mapStudent[456] = "Bob";
+
+    // traversal
+    for(iter = mapStudent.begin(); iter != mapStudent.end(); iter++)
+                cout<<iter->first<<" "<<iter->second<<endl;
+    for(iter_r = mapStudent.rbegin(); iter_r != mapStudent.rend(); iter_r++)
+                cout<<iter_r->first<<" "<<iter_r->second<<endl;
+
+    // find and erase the element
+    iter = mapStudent.find(123);
+    mapStudent.erase(iter);
+
+    iter = mapStudent.find(123);
+
+    if(iter != mapStudent.end())
+       cout << "Found, value is " << iter->second << endl;
+    else
+       cout << "Not Find" << endl;
+
+    return 0;
+}
